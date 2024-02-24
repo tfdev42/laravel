e789f6c09ce1d25d27f6bec366c8a7f0fac8c8fd
tfdev42 <tamas.fazekas.hu@gmail.com>
2024-02-24 15:30:30 +0100

    cart items

--- webshop/database/migrations/2024_02_24_140912_create_carts_table.php
+++ webshop/database/migrations/2024_02_24_140912_create_carts_table.php
@@ -13,6 +13,7 @@ return new class extends Migration {
         Schema::create('carts', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('session_id')->nullable();
             $table->timestamps();
         });
     }
