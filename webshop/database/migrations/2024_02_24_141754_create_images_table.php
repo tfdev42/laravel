de4227bf03c02b90e5bd72fac8147605cc2cfc6e
tfdev42 <tamas.fazekas.hu@gmail.com>
2024-02-24 15:28:02 +0100

    product_variants, cart

--- webshop/database/migrations/2024_02_24_141754_create_images_table.php
+++ webshop/database/migrations/2024_02_24_141754_create_images_table.php
@@ -4,18 +4,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration {
+return new class extends Migration
+{
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('products', function (Blueprint $table) {
+        Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->text('description');
-            $table->unsignedInteger('price');
-            $table->unsignedInteger('main_image_id')->nullable();
             $table->timestamps();
         });
     }
@@ -25,6 +22,6 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        Schema::dropIfExists('products');
+        Schema::dropIfExists('images');
     }
 };
