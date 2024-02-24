da8bb3987ac329fcb2cbbf80e4c94ac98c5589e6
tfdev42 <tamas.fazekas.hu@gmail.com>
2024-02-24 18:42:55 +0100

    products faker

--- webshop/database/factories/ProductVariantFactory.php
+++ webshop/database/factories/ProductVariantFactory.php
@@ -17,7 +17,19 @@ class ProductVariantFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'color' => $this->faker->randomElement([
+                'red',
+                'blue',
+                'green',
+                'orange'
+            ]),
+            'size' => $this->faker->randomElement([
+                'S',
+                'M',
+                'L',
+                'XL',
+                'XXL'
+            ]),
         ];
     }
 }
--- webshop/database/seeders/DatabaseSeeder.php
+++ webshop/database/seeders/DatabaseSeeder.php
@@ -13,6 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        Product::factory(4)->create();
+        Product::factory(4)
+            ->hasVariants(5)
+            ->hasImages(3)
+            ->create();
     }
 }
